d0d3d344b91c08554cef81f32a10e7fc63753260
pujarihemantkumar2003@gmail.com <pujarihemanthkumar2003@gmail.com>
2024-09-21 10:46:42 +0530

    comm

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,6 +3,44 @@ const User = require('../models/user');
 const Address = require('../models/address');
 const router = express.Router();
 
+router.get('/user/:id', async (req, res) => {
+    const userId = req.params.id;
+    
+    try {
+        const user = await User.findOne({
+            where: { id: userId },  // or { name: req.params.name } for finding by name
+            include: Address
+        });
+        
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send('Error occurred: ' + error.message);
+    }
+});
+
+router.get('/user/name/:name', async (req, res) => {
+    const userName = req.params.name;
+
+    try {
+        const user = await User.findOne({
+            where: { name: userName },
+            include: Address
+        });
+
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send('Error occurred: ' + error.message);
+    }
+});
+
 router.post('/submit', async (req, res) => {
     const { name, address } = req.body;
 
